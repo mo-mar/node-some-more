06afc1126ae7d9290bba9a0c967b575fd653183d
Mohamed Omar <50046932+mo-mar@users.noreply.github.com>
2024-04-06 16:14:36 -0600

    serving html, css and js from node

---
+++ server.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const http = require("http");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    const homepagePath = path.join(__dirname, "public", "index.html");
+    fs.readFile(homepagePath, "utf8", (err, content) => {
+      if (err) {
+        throw err;
+      }
+      res.end(content);
+    });
+  }
+
+  // Serve CSS
+
+  if (path.extname(req.url) === ".css") {
+    res.writeHead(200, { "Content-Type": "text/css" });
+    const cssPath = path.join(__dirname, "public", "style.css");
+    fs.readFile(cssPath, "utf8", (err, content) => {
+      if (err) {
+        throw err;
+      }
+      res.end(content);
+    });
+  }
+
+  // Serve JS
+
+  if (path.extname(req.url) === ".js") {
+    res.writeHead(200, { "Content-Type": "text/javascript" });
+    const cssPath = path.join(__dirname, "public", "index.js");
+    fs.readFile(cssPath, "utf8", (err, content) => {
+      if (err) {
+        throw err;
+      }
+      res.end(content);
+    });
+  }
+});
+
+server.listen(5000);
